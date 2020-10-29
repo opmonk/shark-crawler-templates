34340a08e0d698c6d42adf1b31d9cfa0387a2cc8
beckyschu <becky.chu@gmail.com>
2020-10-29 15:13:53 -0700

    Fixed keyword detection in URLs

--- Octoparse/scripts/src/upload_results.sh
+++ Octoparse/scripts/src/upload_results.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-p_results_dir=../results/processed/small
+p_results_dir=../results/processed/medium
 
 files=`ls $p_results_dir`
 awk -F',' ' { print $3 "," $5 "," $6 } ' ../sql/keywords.csv > tmp_keywords.txt
 
 for file in $files
 do
-    run_token=`echo $file | awk -F'.' ' { print $1 } '`
+    # need to allow for '.' in the file name (e.g., l.a.girl)- so matching to .csv
+    run_token=`echo $file | sed 's/.csv//'`
     # keyword is last part of the filename itself. Please note, keyword may contain '-' in it. Need to capture everything AFTER the 3rd field. 
     keyword=`echo $run_token | awk -F'-' ' { print substr($0,index($0,$3)) } '`
 
@@ -23,19 +24,32 @@ do
     if [[ $line == "" ]]
     then
         # For dups, need to find the keyword as the end of the run_token (detect if there is "&catalog" in start url)
-	line=`grep "$keyword"'&' ../sql/keywords.csv`
+	line=`grep "=$keyword"'&' ../sql/keywords.csv`
 
 	if [[ $line == "" ]]
 	then
-    		line=`grep "$keyword"'\/' ../sql/keywords.csv`
+		# e.g., https://www.dhgate.com/wholesale/halter/s011.html#cs-011-1
+    		line=`grep '\/'"$keyword"'\/' ../sql/keywords.csv`
+		
+		# e.g., https://www.dhgate.com/w/eyebrow+pomade.html
+		if [[ $line == "" ]]
+        	then
+			line=`grep '\/'"$keyword"'.html' ../sql/keywords.csv`
+		fi
 	fi
     fi
     assetId=`echo $line | awk -F',' ' { print $3 } '`
     keywordId=`echo $line | awk -F',' ' { print $5 } '`
+	
+    if [[ $assetId != "" && keywordId != "" ]]
+    then
 
-    # echo "$run_token, $assetId, $keywordId"
-    echo "curl -XPOST 'https://tt56usy83j.execute-api.us-east-1.amazonaws.com/dev/crawls' -d'{\"crawlerId\": $crawlerId, \"assetId\": $assetId, \"keywordId\": $keywordId, \"runToken\": \"$run_token\"}'"
-    # curl -XPOST 'https://tt56usy83j.execute-api.us-east-1.amazonaws.com/dev/crawls' -d'{"crawlerId": 8, "assetId": $assetId, "keywordId": $keywordId, "runToken": "$run_token"}'
+        # echo "$run_token, $assetId, $keywordId"
+        echo "curl -XPOST 'https://tt56usy83j.execute-api.us-east-1.amazonaws.com/dev/crawls' -d'{\"crawlerId\": $crawlerId, \"assetId\": $assetId, \"keywordId\": $keywordId, \"runToken\": \"$run_token\"}'"
+        # curl -XPOST 'https://tt56usy83j.execute-api.us-east-1.amazonaws.com/dev/crawls' -d'{"crawlerId": 8, "assetId": $assetId, "keywordId": $keywordId, "runToken": "$run_token"}'
+    else 
+        echo "ERROR: $run_token not found in start urls" >> error.log
+    fi
 done
 
 
