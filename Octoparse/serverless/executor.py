04e3a390f28749f8bae4253605c8861039092d2f
beckyschu <becky.chu@gmail.com>
2021-01-07 18:26:25 -0800

    Serverless with Event Listener changes

--- Octoparse/serverless/executor.py
+++ Octoparse/serverless/executor.py
@@ -1,10 +1,15 @@
 import json
 import postprocessor.parsers.dhgate as dhgate
 
-def main(event, context):
-
-    print("Your numpy array:")
-    dhgate.DHGateParser(['-b', '-i', 'input/DHGate-Production-CB-11042020(2).csv', '-o', 'output/', '-p', 'dhgate']).execute()
+def main(self, argv):
+    """ Input File can either be .csv or .zip
+    Input file should be passed into this function by an event listener.
+    .zip => If .zip extension, then need to unzip the file first and place contents in same directory.
+            Event listener will then be triggered again since new .csv files appear in this diretory.
+    .csv => If .csv extension, then will run through Platform specific parsers.
+    """
+    print("Your Input File:")
+    dhgate.DHGateParser(['-b', '-i', 'input/moon_child_glow.csv', '-o', 'output_moon/', '-p', 'dhgate']).execute()
 
 if __name__ == "__main__":
-    main('', '')
+    main(sys.argv[1:])
